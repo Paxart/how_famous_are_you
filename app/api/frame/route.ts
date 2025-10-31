a7d54f8e91547e0e8918d563a66eb1c7b43b73a0
Paco Franchy <tu_email@loquesea.com>
2025-10-31 14:29:15 +0000

    fix: added content-type json header

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,19 +1,28 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({
-    frames: [
-      {
-        version: "vNext",
-        image: "https://how-famous-are-you.vercel.app/vercel.svg",
-        buttons: [
-          {
-            label: "Generate Yours",
-            action: "post",
-            target: "https://how-famous-are-you.vercel.app/api/frame/next",
-          },
-        ],
+  return new NextResponse(
+    JSON.stringify({
+      frames: [
+        {
+          version: "vNext",
+          image: "https://how-famous-are-you.vercel.app/vercel.svg",
+          buttons: [
+            {
+              label: "Generate Yours",
+              action: "post",
+              target: "https://how-famous-are-you.vercel.app/api/frame/next",
+            },
+          ],
+        },
+      ],
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store",
       },
-    ],
-  });
+    }
+  );
 }
